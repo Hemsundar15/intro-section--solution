920706fd3598bf0bd267d92d220ddebc1a9d1e8e
Hemsundar Paranthaman <hemsundar.paranthaman@outlook.com>
2022-04-16 20:46:39 +0530

    Completed

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import "./scss/main.scss";
 const hamburger = document.querySelector("#hamburger");
 const mobileNav = document.querySelector("#mobilenav");
 
-hamburger.addEventListener("click", () => {
-    let isMenuOpen = false;
+hamburger.src = "./assets/icon-menu.svg";
 
+let isMenuOpen = false;
+
+hamburger.addEventListener("click", () => {
     isMenuOpen ? (isMenuOpen = false) : (isMenuOpen = true);
     mobileNav.classList.toggle("active");
+
     isMenuOpen
         ? (hamburger.src = "./assets/icon-close-menu.svg")
         : (hamburger.src = "./assets/icon-menu.svg");
